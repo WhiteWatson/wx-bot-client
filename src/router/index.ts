87cf9f21ad2e69fcbc8c4c9c7e59f45aece01abd
baiyh2 <baiyh2@ziroom.com>
2023-03-01 15:06:37 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
