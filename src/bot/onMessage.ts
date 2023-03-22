66bb195dd42c797336f7e474a5801af06c7c3032
白钰航 <734004037@qq.com>
2023-03-22 20:57:07 +0800

    feat: gitignore

--- src/bot/onMessage.ts
+++ src/bot/onMessage.ts
@@ -1,15 +1,39 @@
 import { MessageInterface } from "wechaty/impls";
 import { botName } from ".";
-import { firstName } from "./config";
+import { firstName, jiaweisi } from "./config";
 import { sendMessage } from "./chatgpt/main";
 import store from "@/store";
 import { ChatCompletionRequestMessage } from "openai";
 
-const history: any = [];
+let history: any = [];
 
 export const onMessage = async (message: MessageInterface) => {
   if (message.text().startsWith("/ping")) {
     await message.say("pong");
+    return;
+  }
+  if (message.text().startsWith("/clearmemory")) {
+    history = [];
+    message.say(
+      `@${message.from()?.payload?.name} 记忆已清除...`
+    );
+    return;
+  }
+  if (message.text().startsWith("/贾维斯")) {
+    const user_input = jiaweisi;
+    const messages: ChatCompletionRequestMessage[] = [];
+    for (const [input_text, completion_text] of history) {
+      messages.push({ role: "user", content: input_text });
+      messages.push({ role: "assistant", content: completion_text });
+    }
+    messages.push({ role: "user", content: user_input });
+    sendMessage(messages).then((res: any) => {
+      const completion_text = res[0].message.content;
+      message.say(
+        `@${message.from()?.payload?.name} 贾维斯已就绪...`
+      );
+      history.push([user_input, completion_text]);
+    });
     return;
   }
   try {
